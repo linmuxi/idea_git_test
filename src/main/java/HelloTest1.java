55c307c4174a113da51a067e804d831d0514640b
Hunter <2010_lin@sina.cn>
2018-09-25 12:59:57 +0800

    test
    1111

--- src/main/java/HelloTest1.java
+++ src/main/java/HelloTest1.java
@@ -1,6 +1,6 @@
 public class HelloTest1 {
     public static void main(String[] args) {
-        System.out.println(">>>12222211");
+        System.out.println(">>>12222214441");
         int i = 10;
         System.out.println("hello "+i);
     }
