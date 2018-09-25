956ed6402c9b11e35e86f52fd398df5cc80d04c7
Hunter <2010_lin@sina.cn>
2018-09-25 12:59:20 +0800

    测试提交
    testcommit

--- src/main/java/HelloTest1.java
+++ src/main/java/HelloTest1.java
@@ -1,5 +1,7 @@
 public class HelloTest1 {
     public static void main(String[] args) {
         System.out.println(">>>12222211");
+        int i = 10;
+        System.out.println("hello "+i);
     }
 }
